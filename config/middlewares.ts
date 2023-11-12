4483206e150a7f8d02a783d2773319854e10f3df
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-11-12 00:07:58 -0600

    Add AWS support

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,34 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'hb-bucket-2023.s3.us-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'hb-bucket-2023.s3.us-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
