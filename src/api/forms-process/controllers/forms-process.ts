0c31014504c56611d630c2f8077c0a4631f1a878
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-08-12 15:23:36 -0500

    Fix access to fields

--- src/api/forms-process/controllers/forms-process.ts
+++ src/api/forms-process/controllers/forms-process.ts
@@ -11,24 +11,35 @@ export default {
   //   }
   // }
   async formProcess(ctx) {
-  // Check if emailString and phoneNumberString are in formData
-  if (
-    !ctx.request.name || 
-    !ctx.request.body.emailString ||
-    !ctx.request.body.phoneNumberString || !ctx.request.body.token
-  ) {
+    // Check if emailString and phoneNumberString are in formData
+    if (
+      !ctx.request.body.name ||
+      !ctx.request.body.emailString ||
+      !ctx.request.body.phoneNumberString ||
+      !ctx.request.body.token
+    ) {
       return ctx.badRequest("Missing name or emailString or phoneNumberString");
-  }
+    }
 
-  // Adds to DB
-  try {
-    await strapi.service("api::forms-process.forms-process").addUserForm(ctx.request.name, ctx.request.body.emailString, ctx.request.body.phoneNumberString, ctx.request.body.token);
-  } catch (e) {
-    strapi.log.error(e);
-    return ctx.internalServerError("A Whoopsie Happened");
-  }
-  ctx.send({
-    message: 'Form data created.'
-}, 201);
-}
+    // Adds to DB
+    try {
+      await strapi
+        .service("api::forms-process.forms-process")
+        .addUserForm(
+          ctx.request.body.name,
+          ctx.request.body.emailString,
+          ctx.request.body.phoneNumberString,
+          ctx.request.body.token
+        );
+    } catch (e) {
+      strapi.log.error(e);
+      return ctx.internalServerError("A Whoopsie Happened");
+    }
+    ctx.send(
+      {
+        message: "Form data created.",
+      },
+      201
+    );
+  },
 };
