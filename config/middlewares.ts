a31f0c0b259acc0fe9fad8338172e8a25b9ddfb0
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-01-31 12:40:29 -0600

    Add CORS exception for new bucket url

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,27 +1,29 @@
 export default [
-  'strapi::errors',
+  "strapi::errors",
   /* Replace 'strapi::security', with this snippet */
   /* Beginning of snippet */
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'hb-bucket-2023.s3.us-east-1.amazonaws.com',
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "hb-bucket-2023.s3.us-east-1.amazonaws.com",
+            "hb-nutcracker-webapp.s3.us-east-1.amazonaws.com",
           ],
-          'media-src': [
+          "media-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'hb-bucket-2023.s3.us-east-1.amazonaws.com',
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "hb-bucket-2023.s3.us-east-1.amazonaws.com",
+            "hb-nutcracker-webapp.s3.us-east-1.amazonaws.com",
           ],
           upgradeInsecureRequests: null,
         },
@@ -29,12 +31,12 @@ export default [
     },
   },
   /* End of snippet */
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
